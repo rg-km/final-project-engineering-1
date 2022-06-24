2161507e015025408b0e7c0eec918cf37762bedd
muhangga <anggayasdoe11@gmail.com>
2022-06-25 00:57:42 +0700

    feat: crud users, category and fetch content user

--- be/finalproject/web/handler/content.go
+++ be/finalproject/web/handler/content.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"finalproject/module/content"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -20,8 +19,7 @@ func (h *contentHandler) Index(c *gin.Context) {
 
 	contents, err := h.contentService.FetchAllContentAndUser()
 	if err != nil {
-		// c.HTML(http.StatusInternalServerError, "error.html", nil)
-		fmt.Println(err)
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
 		return
 	}
 	c.HTML(http.StatusOK, "content.html", gin.H{"contents": contents})
